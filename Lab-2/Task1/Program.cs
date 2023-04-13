b6d861d65bea5e13c08406db463d2463373a67b0
Svyatuy <112246649+Svyatuy@users.noreply.github.com>
2023-04-13 21:53:32 +0300

    Lab-2
    Lab-2

---
+++ Lab-2/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate int StringLength(string str);
+
+        static void Main(string[] args)
+        {
+            string[] words = { "apple", "banana", "cherry", "date" };
+
+            StringLength stringLength = s => s.Length;
+
+            foreach (string word in words)
+            {
+                int length = stringLength(word);
+                Console.WriteLine($"{word}: {length}");
+            }
+        }
+    }
+}
